290f5e228193e92d1c603e61b5c5d62b74eefda9
ianyeyu <ian.ye.yu.92@gmail.com>
2014-11-13 20:35:00 +0000

    create NN by weights

--- CreateNN2.m
+++ CreateNN2.m
@@ -1,7 +1,8 @@
-function [ NeuNet ] = CreateNN2(HiddLay, trainFunc, output_weights)
+function [ NeuNet ] = CreateNN2(HiddLay, output_weights, examples, binary_targets)
 %To create a NN withs the output weights
     %Configure the NN
-    NeuNet   =   feedforwardnet(HiddLay, trainFunc);
+    [examples,binary_targets] = ANNdata(examples,binary_targets);
+    NeuNet   =   feedforwardnet(HiddLay);
     NeuNet   =   configure(NeuNet, examples, binary_targets);
     %----------------------------------
     %Input the average initial weights
