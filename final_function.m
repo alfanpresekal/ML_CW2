2bafbf79f5f2e47b3f508d1e30c3e540273b8a51
ChiminhTT <0ho.maxence0@gmail.com>
2014-11-14 18:03:18 +0100

    Correction

--- final_function.m
+++ final_function.m
@@ -2,10 +2,10 @@ function [NeuNet,confusion_matrix, f1, recall, precision, CR] = final_function(e
 %To find the best topology with average initial weigths, and opti param 
 %=================================================
 %Find the optimal topology with optimized param for training function
-	[topology, mean_weights, bestF1]    =     cross_fold_topo('traingd(0.1)', epoch, x, y);
+	[topology, mean_weights, bestF1]                 =    cross_fold_topo('traingd(0.1)', epoch, x, y);
 %=================================================
 %Return the optimized Neural Network
-    NeuNet = CreateNN2(topology, 'traingd(0.3)', mean_weights);
-%=========================================
+    NeuNet                                           =    CreateNN2(topology, mean_weights, x, y);
+%================================================= 
 %Get the confusion_matrix and the measures
     [confusion_matrix, f1, recall, precision, CR]    =    cross_fold(NeuNet, x, y);
